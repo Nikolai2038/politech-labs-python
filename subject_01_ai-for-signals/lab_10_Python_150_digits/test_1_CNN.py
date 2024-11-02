a847cf593247ff49745ee4eddefd82b8ddfe7e3c
Nikolai2038 <nikolai2038@yandex.ru>
2024-11-02 10:46:38 +0300

    Format files

--- subject_01_ai-for-signals/lab_10_Python_150_digits/test_1_CNN.py
+++ subject_01_ai-for-signals/lab_10_Python_150_digits/test_1_CNN.py
@@ -1,29 +1,34 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 import tensorflow as tf
 import numpy as np
-from tensorflow.keras import utils
 
-#Подготовка правильных ответов
+# Подготовка правильных ответов
+model = tf.keras.models.load_model("digit_diagnosis.h5")
 
-model = tf.keras.models.load_model('digit_diagnosis.h5')
-
-path = 'image_for_test/digit6024.png'
-#path = 'image_for_test/digit2101.png'
+path = "image_for_test/digit6024.png"
 
 image = tf.keras.preprocessing.image.load_img(path, target_size=(28, 28))
 x = tf.keras.preprocessing.image.img_to_array(image)
 classes = model.predict(np.array([x]))
-print(classes) #Выведет набор вероятностей принадлежности тестового изображения к каждому классу
-print('номер класса, предсказанного нейросетью')
-print(np.argmax(classes)+1)
-#print('вероятность принадлежности тестового изображения к одному из классов')
+
+print('Вероятность принадлежности тестового изображения к одному из классов:')
+print(classes)
+
+print("Номер класса, предсказанного нейросетью:")
+print(np.argmax(classes) + 1)
+
 classes = model.predict(np.array([x]))
-#вывод элемента 3 тензора classes нулевого ранга (т.е. вектора)
-#поскольку элементы в тензоре нумеруются начиная с нуля, фактически элемент будет 4
-print('максимальная вероятность принадлежности')
+
+# Вывод элемента 3 тензора classes нулевого ранга (т.е. вектора)
+# Поскольку элементы в тензоре нумеруются начиная с нуля, фактически элемент будет 4
+print("Максимальная вероятность принадлежности:")
 print(np.max(classes))
-level=0.85 #задание порога распознавания
-print('порог распознавания=',level)
-if np.max(classes)<level:
-	print('ошибка распознавания!')
+
+# Задание порога распознавания
+level = 0.85
+
+print("Порог распознавания = ", level)
+if np.max(classes) < level:
+    print("Ошибка распознавания!")
--- subject_01_ai-for-signals/lab_11_Python_150_NNF_demo/test_1_TNN.py
+++ subject_01_ai-for-signals/lab_11_Python_150_NNF_demo/test_1_TNN.py
@@ -1,22 +1,20 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras import utils
 
-#Подготовка правильных ответов
-#y_train=unils.to_categorical(y_train, 6)
+# Подготовка правильных ответов
+model = tf.keras.models.load_model("plant_diagnosis_3D.h5")
 
-model = tf.keras.models.load_model('plant_diagnosis_3D.h5')
-
-path = 'image_for_test/1.png'
+path = "image_for_test/1.png"
 
 image = tf.keras.preprocessing.image.load_img(path, target_size=(4, 6))
 x = tf.keras.preprocessing.image.img_to_array(image)
 classes = model.predict(np.array([x]))
 
-print(classes) #Выведет набор вероятностей принадлежности тестового изображения к каждому классу
-print('номер класса, предсказанного нейросетью')
-print(np.argmax(classes)+1)
+print('Вероятность принадлежности тестового изображения к одному из классов:')
+print(classes)
 
-
+print("Номер класса, предсказанного нейросетью:")
+print(np.argmax(classes) + 1)
